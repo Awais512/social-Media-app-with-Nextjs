c6d0e08780faa7d43ffc8f93c361605fc7016f33
awais512 <raza.awais25@gmail.com>
2021-05-18 22:43:27 +0500

    api route for Login and Jwt token Implementation

---
+++ api/auth.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/UserModel');
+const Follower = require('../models/FollowerModel');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const isEmail = require('validator/lib/isEmail');
+
+router.post('/', async (req, res) => {
+  const { email, password } = req.body.user;
+
+  if (!isEmail(email)) return res.status(401).send('Invalid email');
+  if (password.length < 6)
+    return res.status(401).send('Password must be at least 6 characters');
+
+  try {
+    const user = await User.findOne({ email: email.toLowerCase() }).select(
+      '+password'
+    );
+    if (!user) {
+      return res.status(401).send('Invalid Credentials');
+    }
+
+    const isPassword = await bcrypt.compare(password, user.password);
+    if (!isPassword) return res.status(401).send('Invalid credentials');
+
+    const payload = { userId: user._id };
+    jwt.sign(
+      payload,
+      process.env.jwtSecret,
+      { expiresIn: '2d' },
+      (err, token) => {
+        if (err) throw err;
+        res.status(200).json(token);
+      }
+    );
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send('Server Error');
+  }
+});
+
+module.exports = router;
